926763d4b79963fc09bdf75263a53ab2bb774a21
Gabriel Gonzalez <gabrielgonzalez@gmail.com>
2025-11-24 19:29:24 -0600

    camilo

---
+++ backend/models/reviewsModel.js
@@ -0,0 +1,12 @@
+//Las reseñas son reseñas para el salon en general como google maps, NO de productos o servicios en especifico
+//El modelo para reseñas debe de tener:
+//puntaje del 0 al 5
+//contenido de la reseña
+//usuario de que escribio la reseña
+//fecha de creacion y ultima modificaciones
+//en los controladores debe de haber metodos para lo siguiente
+//crear una reseña (debe de llenar el campo de usuario que escribio la reseña, obteniendo el nombre desde el token)
+//modificar una reseña ya creada (un usuario solo puede modificar su propia reseña)
+//obtener las reseñas ya creadas
+//eliminar una reseña (un usuaio solo puede borrar su propia reseña, un usuario administrador puede eliminar la que sea)
+
